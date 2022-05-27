3e02edf261a04e4b8278d2290aa0256114ae7be1
sarveshwaran Rajendiran <sarveshwaranrajendiran@AGLS-MLT-429.local>
2022-05-27 15:04:19 +0530

    deleted a variable

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -6,7 +6,6 @@ import axios from "axios"
 import withProgress from "../HigherOrderComponents/WithProgress"
 
 const Login = (props) => {
-    const a=123;
     const [error,setError]=useState(false)
     const [isLoading,setIsLoading]=useState(false)
     const [isTokenGen,setIsTokenGen]= useState(false)
